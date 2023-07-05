689ee453a485d472cb0ccc1359f5da618e8fb325
disc100 <cspector217@gmail.com>
2023-07-05 14:27:09 -0500

    Create playGame function, revise functions

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
+const ROCK = "Rock";
+const PAPER = "Paper";
+const SCISSORS = "Scissors";
+const LOSE = "You lose!";
+const WIN = "You win!";
+const TIE = "Tie game!";
+
 const getComputerChoice = () => {
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = [ROCK, PAPER, SCISSORS];
   const random = Math.floor(Math.random() * choices.length);
   return choices[random];
 };
 
 const getPlayerChoice = () => {
   let choice = prompt("What is your choice?");
+  choice = choice[0].toUpperCase() + choice.slice(1).toLowerCase();
 
-  if (choice[0].toUpperCase() + choice.slice(1).toLowerCase() == "Rock") {
-    return choice[0].toUpperCase() + choice.slice(1).toLowerCase();
-  } else if (
-    choice[0].toUpperCase() + choice.slice(1).toLowerCase() ==
-    "Paper"
-  ) {
-    return choice[0].toUpperCase() + choice.slice(1).toLowerCase();
-  } else if (
-    choice[0].toUpperCase() + choice.slice(1).toLowerCase() ==
-    "Scissors"
-  ) {
-    return choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+  if (choice == ROCK || choice == PAPER || choice == SCISSORS) {
+    return choice;
   } else return getPlayerChoice();
 };
 
-const playRound = (playerChoice, computerChoice) => {
+const playRound = () => {
+  let playerChoice = getPlayerChoice();
+  let computerChoice = getComputerChoice();
+
+  console.log("Player: " + playerChoice);
+  console.log("Computer: " + computerChoice);
+
   if (playerChoice == computerChoice) {
-    return "Tie game!";
-  } else if (playerChoice == "Rock" && computerChoice == "Paper") {
-    return "You lose!";
-  } else if (playerChoice == "Paper" && computerChoice == "Scissors") {
-    return "You lose!";
-  } else if (playerChoice == "Scissors" && computerChoice == "Rock") {
-    return "You lose!";
-  } else return "You win!";
+    return LOSE;
+  } else if (playerChoice == ROCK && computerChoice == PAPER) {
+    return LOSE;
+  } else if (playerChoice == PAPER && computerChoice == SCISSORS) {
+    return LOSE;
+  } else if (playerChoice == SCISSORS && computerChoice == ROCK) {
+    return LOSE;
+  } else return WIN;
 };
 
-let playerChoice = getPlayerChoice();
-let computerChoice = getComputerChoice();
-console.log("Player:" + " " + playerChoice);
-console.log("Computer:" + " " + computerChoice);
+const playGame = () => {
+  let playerScore = 0;
+  let computerScore = 0;
 
-console.log(playRound(playerChoice, computerChoice));
+  for (let i = 0; i < i + 1; i++) {
+    console.log("Round" + " " + (i + 1));
+    let result = playRound();
+
+    console.log(result);
+    if (result == WIN) {
+      playerScore++;
+    } else if (result == LOSE) {
+      computerScore++;
+    }
+    console.log("Score:" + " " + playerScore + " " + computerScore);
+    if (playerScore == 5 || computerScore == 5) {
+      break;
+    }
+    console.log(" ");
+  }
+  if (playerScore > computerScore) {
+    return console.log("Congratulations! You won the game!");
+  } else return console.log("Cowabummer, dude! You lost the game!");
+};
+
+playGame();
